ce91af8a6eec738f096606aa4a70f3a9bf8e99db
akshay7756/almabetter <akshay.walvekar73@gmail.com>
2022-11-20 13:08:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let marks_clss_12 = [91, 82, 63, 84, fail, "not Present"]
+console.log(marks_clss_12)
+console.log(marks_clss_12[0])
+console.log(marks_clss_12[1])
+console.log(marks_clss_12[2])
+console.log(marks_clss_12[3])
+console.log(marks_clss_12[4])
+console.log(marks_clss_12[5])
